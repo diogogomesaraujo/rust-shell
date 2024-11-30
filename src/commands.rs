a1888170eaa33f2e9e66973c4a37620c462e1658
diogogomesaraujo <118282799+diogogomesaraujo@users.noreply.github.com>
2024-11-30 04:48:35 +0000

    cd and clear

---
+++ src/commands.rs
@@ -0,0 +1,25 @@
+// SHELL COMMANDS
+
+use std::{env, path::Path, str::SplitWhitespace};
+
+pub fn cd(args: SplitWhitespace<>) {
+    let new_dir = args.peekable().peek().map_or("/", |x| *x);
+    let root = Path::new(new_dir);
+
+    if let Err(e) = env::set_current_dir(&root) {
+        eprintln!("{}", e);
+    }
+}
+
+pub fn clear() {
+    match term_size::dimensions() {
+        Some((_, h)) => {
+            for i in 0..h {
+                println!();
+            }
+        },
+        None => {
+            println!("Unable to get the window size!");
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,58 @@
+use std::{io::stdin, process::Command};
+use std::env::{self, current_dir};
+use chrono::prelude::*;
+use std::io::Write;
+
+mod commands;
+
+fn red_text(s: String) -> String {
+    let mut aux: String = String::from("\u{001b}[31m");
+    aux.push_str(&s);
+    aux.push_str("\u{001b}[0m");
+
+    aux
+}
+
+fn teal_text(s: String) -> String {
+    let mut aux: String = String::from("\u{001b}[36m");
+    aux.push_str(&s);
+    aux.push_str("\u{001b}[0m");
+
+    aux
+}
+
+fn main() {
+    commands::clear();
+    let prefix: String = red_text(String::from("kitten"));
+
+    loop {
+        let current_dir = current_dir().unwrap();
+        let time = Local::now().format("%H:%M%P");
+
+        println!("[ {0} {1} ]: {2}", prefix, time, teal_text(current_dir.display().to_string()));
+
+        print!("{}", teal_text(String::from("$ ")));
+        std::io::stdout().flush().unwrap();
+        
+        let mut input: String = String::new();
+        stdin().read_line(&mut input).unwrap();
+
+        let mut parts = input.trim().split_whitespace();
+        let command = parts.next().unwrap();
+        let args = parts;
+
+        match (command, &args) {
+            ("cd", _) => { commands::cd(args); }
+            ("clear", _) => { commands::clear(); },
+
+            (_, _) => {
+                let child = Command::new(command).args(args).spawn();
+
+                match child {
+                    Ok(mut child) => { child.wait().unwrap(); },
+                    Err(e) => { println!("{e}"); }
+                }
+            }
+        }
+    }
+}
